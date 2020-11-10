27546ca46780c8e17625f7c384b5027d582f5d7c
samliu000 <samuel.liu1004@gmail.com>
2020-11-09 16:22:47 -0800

    Commit before Steph merge

--- app/src/main/java/com/example/moodybuds/MainActivity.java
+++ app/src/main/java/com/example/moodybuds/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.moodybuds;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
     }
+
+
 }
