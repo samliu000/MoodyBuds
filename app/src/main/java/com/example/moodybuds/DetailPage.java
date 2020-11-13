d65c36c0686d66bf000dac77cfc632334fb693aa
stephaniedeleon <54347952+stefrdeleon3@users.noreply.github.com>
2020-11-12 16:38:49 -0800

    Partial switch code

--- app/src/main/java/com/example/moodybuds/DetailPage.java
+++ app/src/main/java/com/example/moodybuds/DetailPage.java
@@ -10,9 +10,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.SeekBar;
+import android.widget.Switch;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -42,6 +44,8 @@ public class DetailPage extends AppCompatActivity {
     FirebaseUser currentFirebaseUser;
     DatabaseReference mUserRef;
     ProfileCard userInfo;
+    Switch toTalkSwitch;
+    ImageView alert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,6 +67,20 @@ public class DetailPage extends AppCompatActivity {
         context = this;
         currentFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
         mUserRef = FirebaseDatabase.getInstance().getReference().child("users").child(currentFirebaseUser.getUid());
+        //switch
+        toTalkSwitch = findViewById(R.id.toTalk);
+        alert = findViewById(R.id.alert);
+
+        //switch button
+        toTalkSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    alert.setVisibility(View.VISIBLE);
+                } else {
+                    alert.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
 
         // on click update button
         updateButton.setOnClickListener(new View.OnClickListener() {
